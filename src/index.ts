fa100567bea403c005d0aa85a1059d9e137b0e09
JoaSanchezEdemsa <sanchezcerdanedemsa@gmail.com>
2024-10-01 10:02:24 -0300

    Proyecto iniciado

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 8080;
+
+app.get('/', (req,res) => {
+    console.log("hola");
+    res.send("hola");
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
